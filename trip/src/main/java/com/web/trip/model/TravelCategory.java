676ac5bbb7c3f24cda4a0290ea17f5ea76435c5e
jusoyeon <jusoyeon@localhost>
2017-11-27 13:26:37 +0900

    순서만 바꿈

--- trip/src/main/java/com/web/trip/model/TravelCategory.java
+++ trip/src/main/java/com/web/trip/model/TravelCategory.java
@@ -2,8 +2,8 @@ package com.web.trip.model;
 
 public class TravelCategory {
 	private int travel_cate_num;
-	private String travel_cate_state;
 	private String travel_cate_city;
+	private String travel_cate_state;
 
 	public int getTravel_cate_num() {
 		return travel_cate_num;
@@ -13,20 +13,20 @@ public class TravelCategory {
 		this.travel_cate_num = travel_cate_num;
 	}
 
-	public String getTravel_cate_state() {
-		return travel_cate_state;
-	}
-
-	public void setTravel_cate_state(String travel_cate_state) {
-		this.travel_cate_state = travel_cate_state;
-	}
-
 	public String getTravel_cate_city() {
 		return travel_cate_city;
 	}
 
 	public void setTravel_cate_city(String travel_cate_city) {
 		this.travel_cate_city = travel_cate_city;
+	}
+
+	public String getTravel_cate_state() {
+		return travel_cate_state;
+	}
+
+	public void setTravel_cate_state(String travel_cate_state) {
+		this.travel_cate_state = travel_cate_state;
 	}
 
 }
