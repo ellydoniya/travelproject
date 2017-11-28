83c3977cc48901cd88a286f77af8c822c4f7fb21
Elly <Elly@Doniya>
2017-11-28 12:43:39 +0900

    error 해결

--- trip/src/main/java/com/web/trip/HomeController.java
+++ trip/src/main/java/com/web/trip/HomeController.java
@@ -1,31 +1,31 @@
 package com.web.trip;
 
-
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * Handles requests for the application home page.
- */
 @Controller
 public class HomeController {
 	
-	/*private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-	*//**
-	 * Simply selects the home view to render by returning its name.
-	 *//*
+	// 서버 접속 컴퓨터에 찍어줄지 말지 고민중.
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
+		/*logger.info("Welcome home! The client locale is {}.", locale);
 		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
 		String formattedDate = dateFormat.format(date);
+		model.addAttribute("serverTime", formattedDate );*/
 		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
-	}*/
+		return "index";
+	}
 	
 }
